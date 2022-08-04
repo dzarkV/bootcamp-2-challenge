97f9469a6380c4522dd9fbcdd87a9c9c02e74a3d
giocrisrai <giocrisrai@gmail.com>
2022-08-04 00:02:25 -0400

    ISSUE_06: Se genera el script en bash para resolver el segundo ejercicio

--- 01/ejercicio-2-Manejo-de-contenido-de-archivos.sh
+++ 01/ejercicio-2-Manejo-de-contenido-de-archivos.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+if [[ -f foo/dummy/file1.txt && -f file2.txt ]]
+then
+    cat foo/dummy/file1.txt >> foo/dummy/file2.txt
+    mv foo/dummy/file2.txt foo/empty/.
+else
+    echo "Los archivos no se encuentran en la carpeta"
+fi
