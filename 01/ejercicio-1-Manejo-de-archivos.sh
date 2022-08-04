4bfca107348e7c8b80b474db85f93eeda954a696
giocrisrai <giocrisrai@gmail.com>
2022-08-03 23:34:33 -0400

    ISSUE_02: Se crea el script de bash para el primer ejercicio

---
+++ 01/ejercicio-1-Manejo-de-archivos.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ ! -d foo ]]
+then
+    mkdir foo
+    cd foo
+    mkdir dummy empty
+    cd dummy
+    touch file1.txt file2.txt
+    echo "Me encanta bash!!" >> file1.txt
+else
+    echo "La carpeta foo ya fue creada"
